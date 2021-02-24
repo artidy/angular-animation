2fb068b8ce53a00aa6689bdf294f9dee137ccd5a
Andrey Pechennikov <ap@devandy.me>
2021-02-24 21:56:26 +0600

    create animation angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { boxAnimations } from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: boxAnimations
 })
 export class AppComponent {
-  
+  boxState = 'start'
+  visible = true
+
+  animate() {
+    this.boxState = this.boxState !== 'end' ? 'end' : 'start'
+  }
+
+  animationStarted(event: AnimationEvent) {
+    console.log(event)
+  }
+
+  animationEnd(event: AnimationEvent) {
+    console.log(event)
+  }
 }
